fb9bbdb59f9b902ce20c57adcc4ea03b3cb98f47
kata <kataocean@gmail.com>
2018-06-06 15:17:15 +0900

    first commit.

---
+++ Assets/Scenes/InputManager/Scripts/InputState.cs
@@ -0,0 +1,13 @@
+namespace Girigiri.Input
+{
+
+    public struct InputState
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Enter { get; set; }
+        public bool Cancel { get; set; }
+
+    }
+
+}
